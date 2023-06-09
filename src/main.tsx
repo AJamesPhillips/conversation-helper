a114e93152e642fc2fa3ab5091af3a9fc2b4c087
Alexander James Phillips <ajp.global@gmail.com>
2023-06-09 14:47:54 +0100

    Revert "Failed attempt at using preact with react-redux"
    This reverts commit 52acd96815545916689d35ec0a83c372dfe06bfb.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { render } from "preact"
 import { Provider } from "react-redux"
 
+import { App } from "./app.tsx"
+import "./index.css"
+import { store } from "./state/store.ts"
+
+
+const el_app: HTMLElement = document.getElementById("app")!
 
 render(
-    <Provider>
-        <div />
+    <Provider store={store}>
+        <App />
     </Provider>
-, document.getElementById("app") as HTMLElement)
+, el_app)
